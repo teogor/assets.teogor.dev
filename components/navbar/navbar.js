27342adcb0d891be98eda1b148dcade27ace4291
Teodor G <source@teogor.dev>
2023-04-18 21:09:50 +0300

    created navbar component

---
+++ components/navbar/navbar.js
@@ -0,0 +1,13 @@
+const xhttp = new XMLHttpRequest();
+
+// Define the callback function
+xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        // Insert the navbar HTML into the #navbar element
+        document.getElementById("navbar").innerHTML = this.responseText;
+    }
+};
+
+// Open the file and send the request
+xhttp.open("GET", "/components/navbar/navbar.html", true);
+xhttp.send();
